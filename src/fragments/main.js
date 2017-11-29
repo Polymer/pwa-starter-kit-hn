52533a7f4e6c86b604ddda69c4038e6030d5734a
Keanu Lee <keanulee@google.com>
2017-11-29 15:20:44 -0800

    Update document.title

--- src/fragments/main.js
+++ src/fragments/main.js
@@ -1,4 +1,5 @@
 import '../js/router.js';
+import '../js/metadata.js';
 import { HnAppElement } from '../elements/hn-app-element.js';
 
 // Define element only after adding the reducers it needs (the location reducer)
